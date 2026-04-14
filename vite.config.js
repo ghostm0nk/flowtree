ae32540984e57bb569838cabd4aafc5697d3845f
Arvin Raed <139760390+ghostm0nk@users.noreply.github.com>
2026-04-14 22:15:47 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  server: { host: "0.0.0.0", allowedHosts: true,
-    port: 5173,
-  },
-  build: {
-    esbuild: true,
-  },
-});
+  server: { host: "0.0.0.0", allowedHosts: true },
+  plugins: [react()]
+})
