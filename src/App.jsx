8b4a86a3f9d5071b80982dbbeaf6ec4307d96bb7
Arvin Raed <139760390+ghostm0nk@users.noreply.github.com>
2026-04-14 23:56:30 +0600

    Update src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
-import { Navigate, Outlet } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
 
 function App() {
   return (
-    <div className="app-container">
-      <Outlet />
-    </div>
-  )
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="*" element={<div className="container">404 - Not Found</div>} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
