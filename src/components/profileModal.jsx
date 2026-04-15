5f94d669949bab0854c05c6972b74c499c358a8b
Arvin Raed <139760390+ghostm0nk@users.noreply.github.com>
2026-04-15 23:21:11 +0600

    Add src/components/profileModal.jsx

---
+++ src/components/profileModal.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const ProfileModal = ({ session, onClose }) => {
+  // Component implementation here
+  return (
+    // JSX here
+  );
+};
+
+export default ProfileModal;
